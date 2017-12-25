2d2e90483321b78682202e73f05eb4eb888b6fb4
David Baak <davidbaak@gmail.com>
2017-12-25 15:15:14 +0100

    last remarks about refactoring

--- fizzbuzz/listcomprehension/FizzBuzz.hs
+++ fizzbuzz/listcomprehension/FizzBuzz.hs
@@ -1,5 +1,7 @@
 module FizzBuzz where
 
+-- This can probably be refactored but at this moment I don't know how to encapsulate the functions 
+-- or defenitions that would come from refactoring
 fizzbuzz numbers = [if number `mod` 3 == 0 && number `mod` 5 == 0 then "FizzBuzz"
                     else if number `mod` 3 == 0 then "Fizz"
                     else if number `mod` 5 == 0 then "Buzz"
