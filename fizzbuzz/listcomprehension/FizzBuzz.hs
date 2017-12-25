1d74b6027fe55db7bdb7c9d86799bea19afe2a40
David Baak <davidbaak@gmail.com>
2017-12-25 14:53:35 +0100

    green

--- fizzbuzz/listcomprehension/FizzBuzz.hs
+++ fizzbuzz/listcomprehension/FizzBuzz.hs
@@ -1,4 +1,5 @@
 module FizzBuzz where
 
 fizzbuzz numbers = [if number `mod` 3 == 0 then "Fizz"
+                    else if number == 5 then "Buzz"
                     else show number | number <- numbers]
