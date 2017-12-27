70c80761bb6d00e4141a2bb663db996340c0ad20
David Baak <davidbaak@gmail.com>
2017-12-27 10:29:53 +0100

    added alternative

--- fizzbuzz/listcomprehension/FizzBuzz.hs
+++ fizzbuzz/listcomprehension/FizzBuzz.hs
@@ -8,3 +8,10 @@ fizzbuzz numbers = [if number `mod` 3 == 0 && number `mod` 5 == 0 then "FizzBuzz
                     else if number `mod` 5 == 0 then "Buzz"
                     else show number | 
                     number <- numbers, number >=1 && number <=100]
+
+-- Alternative which simply prints the output [1..100] but does not allow input
+fizzbuzz' = [if number `mod` 3 == 0 && number `mod` 5 == 0 then "FizzBuzz"
+                    else if number `mod` 3 == 0 then "Fizz"
+                    else if number `mod` 5 == 0 then "Buzz"
+                    else show number | 
+                    number <- [1..100]]
