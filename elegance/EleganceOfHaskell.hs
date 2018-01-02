339a02b64c62081381c466f7cbcdd6fdee6f35e7
David Baak <davidbaak@gmail.com>
2018-01-02 21:43:54 +0100

    boy scout rule: added type declaration

--- elegance/EleganceOfHaskell.hs
+++ elegance/EleganceOfHaskell.hs
@@ -1,5 +1,8 @@
+multiplicationTableOneThroughTen :: [Int]
 multiplicationTableOneThroughTen = [x*y | x <- [1..10], y <- [1..10]]
 
+multiplicationTableOfAPositiveRangeLimitedToMultiplicationByTen :: [Int] -> [Int]
 multiplicationTableOfAPositiveRangeLimitedToMultiplicationByTen range = [x*y | x <- range, y <- [1..10], x >= 1]
 
+multiplicationTableOfAnyPositiveRange :: [Integer] -> [Integer]
 multiplicationTableOfAnyPositiveRange range = [x*y | x <- range, y <- range, x >= 1 && y >= 1]
--- fizzbuzz/listcomprehension/FizzBuzz.hs
+++ fizzbuzz/listcomprehension/FizzBuzz.hs
@@ -1,14 +1,17 @@
 module FizzBuzz where
 
+showAll :: [[Char]]
 showAll = fizzbuzz [1..100]
+
 -- This can probably be refactored but at this moment I don't know how to encapsulate the functions 
 -- or definitions that would come from refactoring
+fizzbuzz :: [Int] -> [[Char]]
 fizzbuzz numbers = [if number `mod` 3 == 0 && number `mod` 5 == 0 then "FizzBuzz"
                     else if number `mod` 3 == 0 then "Fizz"
                     else if number `mod` 5 == 0 then "Buzz"
                     else show number | 
                     number <- numbers, number >=1 && number <=100]
-
+fizzbuzz' :: [[Char]]
 -- Alternative which simply prints the output for [1..100] but does not allow input
 fizzbuzz' = [if number `mod` 3 == 0 && number `mod` 5 == 0 then "FizzBuzz"
                     else if number `mod` 3 == 0 then "Fizz"
