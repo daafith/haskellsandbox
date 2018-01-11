5be7c2a65d987e67acc8d8aae8d4dec4071049c6
David Baak <davidbaak@gmail.com>
2018-01-11 13:57:44 +0100

    added pattern matching example

---
+++ fizzbuzz/patternmatching/FizzBuzz.hs
@@ -0,0 +1,9 @@
+module FizzBuzzPatternMatching where
+
+main = [fizzbuzz x | x <- [1..100]]
+      where fizzbuzz number
+             | number `isMultipleOf` [3, 5] = "FizzBuzz"
+             | number `isMultipleOf` [3]    = "Fizz"
+             | number `isMultipleOf` [5]    = "Buzz" 
+             | otherwise                    = show number
+              where x `isMultipleOf` ys = all (\y -> x `mod` y == 0) ys
---
+++ fizzbuzz/patternmatching/README.md
@@ -0,0 +1,10 @@
+# FizzBuzz Pattern Matching
+Implementation of FizzBuzz using pattern matching.
+
+## Run fizzbuzz
+Use the [GHCi](http://downloads.haskell.org/~ghc/latest/docs/html/users_guide/ghci.html) to load the module and run the fizzbuzz
+```
+$ ghci
+Prelude> :l FizzBuzz.hs
+*FizzBuzz> main
+```
