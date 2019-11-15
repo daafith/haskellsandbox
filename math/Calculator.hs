68514bc37b3eb2bffe0cb114d375cb3a2431a910
daafith <davidbaak@gmail.com>
2019-11-15 08:52:01 +0100

    fixed reference

--- math/Calculator.hs
+++ math/Calculator.hs
@@ -5,7 +5,7 @@ fac n = if n == 0 then 1 else n * fac (n-1)
 
 facPat :: (Eq p, Num p) => p -> p
 facPat 0 = 1
-facPat n = n * fac (n-1)
+facPat n = n * facPat (n-1)
 
 fib :: (Eq a, Num a) => a -> a
 fib n = if n == 0 || n == 1 then n else fib (n-1) + fib (n-2)
@@ -13,7 +13,7 @@ fib n = if n == 0 || n == 1 then n else fib (n-1) + fib (n-2)
 fibPat :: (Eq a, Num a) => a -> a
 fibPat 0 = 0
 fibPat 1 = 1
-fibPat n = fib (n-1) + fib (n-2)
+fibPat n = fibPat (n-1) + fibPat (n-2)
 
 fib' :: (Num a, Enum a, Ord a) => a -> [a]
 fib' n = [fibPat x | x <- [0..n], x >= 0]
